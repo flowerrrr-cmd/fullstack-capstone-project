9b8460e6d040cfe4c75f945eca6dd233e2ca10a3
simple123869 <nagmanayak166@gmail.com>
2026-04-18 22:31:33 +0530

    add findOne in authRoutes

--- authRoutes.js
+++ authRoutes.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const connectToDatabase = require('./db');
 
-router.post('/api/register', (req, res) => {
+router.post('/api/register', async (req, res) => {
     res.json({ message: "User registered" });
 });
 
-router.post('/api/login', (req, res) => {
-    res.json({ token: "12345" });
+router.post('/api/login', async (req, res) => {
+    const { username } = req.body;
+
+    const db = await connectToDatabase();
+    const collection = db.collection('users');
+
+    const user = await collection.findOne({ username });
+
+    if (user) {
+        res.json({ message: "User found", user });
+    } else {
+        res.json({ message: "User not found" });
+    }
 });
 
-router.put('/api/user', (req, res) => {
+router.put('/api/user', async (req, res) => {
     res.json({ message: "User updated" });
 });
 
